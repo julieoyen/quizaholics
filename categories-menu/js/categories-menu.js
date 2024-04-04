abf4dbdff0497b6039deda1df1e9563857c36769
fillip husebø <felipehusebo@gmail.com>
2024-04-04 13:05:16 +0200

    added selector button functionality

--- categories-menu/js/categories-menu.js
+++ categories-menu/js/categories-menu.js
@@ -1,9 +1,22 @@
-const selectorButtons = document.querySelectorAll(".gameModeButton");
+const selectorWrapper = document.querySelector(".gameModeButtonsWrapper");
 
-const selectedGameMode = function(event) {
-    event.target.style.backgroundColor = "blue";
+if (selectorWrapper) {
+    let selectorButtons = selectorWrapper.querySelectorAll(".gameModeButton");
+    let i;
+    for (i=0; i < selectorButtons.length; i++) {
+        selectorButtons[i].addEventListener("click", function () {
+            if (!this.classList.contains('active')) {
+                clearActives(selectorButtons);
+                this.classList.toggle('active');
+            }
+        });
+    }
 }
 
-selectorButtons.forEach(button => {
-    button.addEventListener("click", selectedGameMode);
-});
+function clearActives(classlist) {
+    if (classlist) {
+        for (i = 0; i < classlist.length; i++) {
+            classlist[i].classList.remove('active');
+        }
+    }
+}
